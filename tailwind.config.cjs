948b023fb67b77038dc276a20573795c37ce8d0c
Frail Bongat <frail.bongat@gmail.com>
2022-07-11 00:06:22 +0800

    initial: start project

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					400: '#214B6B',
+					500: '#1A3A53',
+					800: '#1B2837',
+					900: '#17202D'
+				}
+			}
+		}
+	},
+	plugins: []
+}
